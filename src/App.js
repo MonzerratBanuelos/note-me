468c036c7db6cccbb1af6591998b000f7785244b
MonzerratBanuelos <ambgmonze@gmail.com>
2022-04-21 16:33:42 -0500

    Se trabajo en el Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
-import Login from './component/Login';
-import Register from './component/Register';
-import Feed from './component/Feed';
-import About from './component/About';
-// import Page404 from './component/Page404';
+import Login from './views/Login';
+import Register from './views/Register';
+import Feed from './views/Feed';
+import About from './views/About';
+import Page404 from './views/Page404';
 
+import Navigation from './Components/Navigation';
 function App() {
   return (
     <BrowserRouter>
+
+    <Navigation/> 
+
       <Routes>
         <Route path='note-me/' element={<Login/>} />
         <Route path='note-me/Register' element= {<Register/>} />
         <Route path='note-me/Feed' element = {<Feed/>} />
         <Route path='note-me/About' element={<About/>} />
+        <Route path='*' element={<Page404/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Navigation.js
@@ -0,0 +1,23 @@
+import {NavLink} from 'react-router-dom'
+
+function Navigation () {
+    return (
+        <div>
+            <ul>
+                <button>
+                    <NavLink to='note-me/feed'>Home</NavLink>
+                </button>
+                <li>
+                    <NavLink to='note-me/Register'>Register</NavLink>
+                </li>
+                <li>
+                    <NavLink to='note-me/Feed'>Feed</NavLink>
+                </li>
+                <li>
+                    <NavLink to='note-me/About'>About</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Navigation;
