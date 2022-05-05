a00a7c4af89cc1bde25965256d3721cfd8917b55
MonzerratBanuelos <ambgmonze@gmail.com>
2022-05-05 16:42:52 -0500

    Se añadió la funcionalidad de eliminar una nota con el uso de DeleteDoc de firebase. Se modifico el css grid de la ruta Feed que muestra las notas

--- src/Component-Routes/Feed/Feed.jsx
+++ src/Component-Routes/Feed/Feed.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-key */
 import './Feed.css'
-import { useEffect, useState} from 'react';
-import { collection, onSnapshot } from '../../firebase-store/firebase-imports';
 import exitImg from '../../resource/exitImg.png';
 import addSign from '../../resource/addSign.png';
 import Pencil from '../../resource/Pencil.png';
 import Trash from '../../resource/Trash.png'
+import { useEffect, useState} from 'react';
+import { collection, onSnapshot, doc, deleteDoc} from '../../firebase-store/firebase-imports';
 import { db, auth } from '../../firebase-store/firebaseKeys';
 import { useNavigate } from 'react-router-dom';
 
@@ -22,49 +22,52 @@ function Feed({handleExit, UserInfo}) {
         documents.push({...note.data(), id: note.id});
       });
     setNoteList(documents)
-  });
-};
+    });
+  };
 
   useEffect(() => {
     getNotes()
   }, []);
+
+  const deleteNote = async (id) =>{
+    await deleteDoc(doc(db, 'users', auth.currentUser.uid, 'notes',id))
+  }
   
 
   return (
     <div className='feedPage'>
+
       <header>
-      <img src={UserInfo.photoURL} className='User-photo' alt='logo' />
-      <h3>{UserInfo.displayName}</h3>
-      <button className='LogOut' onClick={()=>{
-        handleExit().then(()=>{console.log('good bye')})
-      }}>
-      <img src={exitImg} className='exit-Logo' alt='Exit' />
-      </button>
+        <img src={UserInfo.photoURL} className='User-photo' alt='logo' />
+        <h3>{UserInfo.displayName}</h3>
+        <button className='LogOut' onClick={()=>{
+          handleExit().then(()=>{console.log('good bye')})
+        }}>
+        <img src={exitImg} className='exit-Logo' alt='Exit' />
+        </button>
       </header>
+
       <div className='notesContainer'>
-      {noteList.map((post)=> {
-        return <section className='noteContainer' key={post.id}>
-        <section className='btnSection'>
+        {noteList.map((post)=> {
+          return <section className='noteContainer' key={post.id}>
+          <h1 className='noteTitle'>{post.title}</h1>
+          <p className='noteContent'>{post.note}</p>
+          <p className='noteAuthor'>Author:{post.author}</p>
+          <section className='btnSection'>
         <button className='editButton' onClick={() => {console.log('En un futuro editaré')}}>
-      <img src={Pencil} className='editImgBtn' alt='Editbtn' />
-      </button>
-        <button className='eraseButton' onClick={() => {console.log('En un futuro eliminaré')}}>
+        <img src={Pencil} className='editImgBtn' alt='Editbtn' />
+        </button>
+        <button className='eraseButton' onClick={() => {deleteNote(post.id)}}>
         <img src={Trash} className='eraseImgBtn' alt='Erasebtn' />
         </button>
         </section>
-          <h1 className='noteTitle'>{post.title}</h1>
-          <p className='noteContent'>{post.note}</p>
-      <p className='noteAuthor'>Author:{post.author}</p>
           </section>;
-      
       })};
-      
-      
       </div>
+
       <button className='addNoteBtn' onClick={()=> {navigate('/MakeNote')}}>
       <img src={ addSign } className='addBtn' alt='addBtn' />
       </button>
-      
     </div>
   );
 }
