7ce43efaa88cc0c68a5f204a939228905e4d160d
MonzerratBanuelos <ambgmonze@gmail.com>
2022-05-09 12:57:14 -0500

    Se inciaron pruebas de testeo sobre el componenete 404Page

--- src/Component-Routes/Page404/Page404.test.js
+++ src/Component-Routes/Page404/Page404.test.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-undef */
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Page404 from './Page404'
 
 describe('Page 404 component', () => {
     it('render the content', () => {
         render(<Page404/>)
     })
+})
+
+it('sould navigate', () => {
+    render(<Page404/>)
 })
